76965870900b3ee730682c718eebb7c9b75fa8ae
sougata <sdas15226@gmail,com>
2022-07-23 20:22:04 +0530

    List orders for admin

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -6,6 +6,18 @@ import Product from '../models/productModel.js';
 import { isAuth, isAdmin } from '../utils.js';
 
 const orderRouter = express.Router();
+
+//  to return All orders details required fo the orderlist Screen
+orderRouter.get(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find().populate('user', 'name');
+    res.send(orders);
+  })
+);
+
 orderRouter.post(
   '/',
   isAuth,
